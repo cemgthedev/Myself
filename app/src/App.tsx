6d9aa19676db9e9f1e820dc40cd3cb34c7e10a9a
cemgthedev <cemg.the.dev@gmail.com>
2023-03-21 13:22:46 -0300

    preparando projeto para desenvolvimento (criação do projeto vite, instalação e configuração do styled components)

---
+++ app/src/App.tsx
@@ -0,0 +1,9 @@
+import {Title} from './styles/global';
+
+export function App() {
+  return (
+    <Title>
+      Hello World
+    </Title>
+  );
+}
---
+++ app/src/styles/global.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Div = styled.div`
+  display: flex;
+  justify-content: center;
+  background-color: papayawhip;
+`;
+
+export const Title = styled.h1`
+  font-size: xx-large;
+  color: chocolate;
+  text-align: center;
+  border: solid 2pt chocolate;
+`;
